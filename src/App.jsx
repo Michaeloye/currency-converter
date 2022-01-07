e38208f904c3db1698689bfae35be65ad0b71cc8
michaeloye <michaeloye23@gmail.com>
2022-01-07 20:58:08 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-white">
+      <h1 className="text-black text-2xl">HELLO, I'M A REACT DEVELOPER!</h1>
+      <p className="text-black">- Rohan Singh Rawat</p>
+    </div>
+  );
+}
+
+export default App;
