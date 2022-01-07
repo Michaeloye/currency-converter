094b311f68bb8bbd17b1a62bc37cba1a01fa92a8
michaeloye <michaeloye23@gmail.com>
2022-01-07 22:27:12 +0100

    add colors and boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-red": "#C91515",
+      },
+      boxShadow: {
+        light: "0 34px 59px 0 rgba(218, 218, 218, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
