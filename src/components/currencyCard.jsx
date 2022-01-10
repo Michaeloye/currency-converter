b256a98113e5fa983377b599f6f36e7bb3d42a52
michaeloye <michaeloye23@gmail.com>
2022-01-10 16:48:55 +0100

    add flag image url

--- src/components/currencyCard.jsx
+++ src/components/currencyCard.jsx
@@ -1,17 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function CurrencyCard({ countryFlag }) {
+function CurrencyCard() {
   const [countries, setCountries] = useState([]);
+  const [flagURL, setFlagURL] = useState("https://countryflagsapi.com/svg/US");
+  const [inputValue, setInputValue] = useState("USD - US Dollar");
+  const [identifier, setIdentifier] = useState("");
+  const [currencyCode, setCurrencyCode] = useState([{ symbol: "$" }]);
+  const [imageAvailable, setImageAvailable] = useState(true);
 
+  function setCountry(country) {
+    const url = "https://countryflagsapi.com/svg/";
+    const filterCountryCode = countries.filter(
+      (cout) => cout.code === country.slice(0, 3)
+    );
+    setCurrencyCode(filterCountryCode);
+
+    setIdentifier(country.slice(0, 2));
+    setFlagURL(url + identifier);
+    setInputValue(country);
+
+    // check if flag image exists
+
+    const img = new Image();
+    img.src = url + identifier;
+
+    if (img.complete) {
+      setImageAvailable(true);
+    } else {
+      img.onload = () => {
+        setImageAvailable(true);
+      };
+
+      img.onerror = () => {
+        setImageAvailable(false);
+      };
+    }
+  }
   useEffect(() => {
     axios
       .get(
-        "https://gist.githubusercontent.com/Kol007/e734eca54c241324f49691d6ed3424e4/raw/f621703926fc13be4f618fb4a058d0454177cceb/countries.json"
+        "https://raw.githubusercontent.com/Michaeloye/currencies-name-and-code/main/currency-list.json"
       )
       .then((response) => {
-        console.log(response);
-        setCountries(response.data.countries.country);
+        setCountries(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -24,22 +56,43 @@ function CurrencyCard({ countryFlag }) {
     >
       {/* Inputs */}
       <div className="ml-5 flex flex-col gap-5">
-        <input type="text" className="border-2 h-10 px-2 rounded-lg w-8/12" />
+        {/* Input amount */}
+        <span>
+          {currencyCode.length ? (
+            <span className="mr-1">
+              {currencyCode.map((code) => code.symbol)}
+            </span>
+          ) : (
+            ""
+          )}
+          <input
+            type="text"
+            inputMode="decimal"
+            autoComplete="off"
+            className="border-2 h-10 px-2 rounded-lg w-8/12"
+          />
+        </span>
         <div className="flex flex-row items-center gap-5">
           <input
             type="text"
             className="border-2 h-10 px-2 rounded-lg w-8/12"
             list="countries"
+            value={inputValue}
+            placeholder="Type to search..."
+            onClick={() => setInputValue("")}
+            onChange={(e) => setCountry(e.target.value)}
           />
           <datalist id="countries">
             {countries.map((country) => (
-              <option key={country.countryName}>
-                {country.currencyCode} - {country.countryName}
+              <option key={country.name}>
+                {country.code} - {country.name}
               </option>
             ))}
           </datalist>
 
-          <img src={countryFlag} alt="Country flag" height={48} width={48} />
+          {imageAvailable && (
+            <img src={flagURL} alt="Country flag" height={48} width={48} />
+          )}
         </div>
       </div>
     </div>
