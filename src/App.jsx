6800921f502882c8e650ecbeb12f3f9446bca65e
michaeloye <michaeloye23@gmail.com>
2022-01-11 01:31:44 +0100

    add axios to make get requests; update variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,147 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/header";
 import CurrencyCard from "./components/currencyCard";
 import { CgArrowsExchange } from "react-icons/cg";
-
+import axios from "axios";
+const headers = {
+  headers: { "X-Api-Key": import.meta.env.VITE_APP_API_KEY_2 },
+};
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [amountInput, setAmountInput] = useState("");
+  const [amountOutput, setAmountOutput] = useState("");
+
+  const [flagURL, setFlagURL] = useState("https://countryflagsapi.com/svg/US");
+  const [secondFlagURL, setSecondFlagURL] = useState(
+    "https://countryflagsapi.com/svg/US"
+  );
+
+  const [inputValue, setInputValue] = useState("USD - US Dollar");
+  const [secondInputValue, setSecondInputValue] = useState("USD - US Dollar");
+
+  const [identifier, setIdentifier] = useState("");
+  const [secondIdentifier, setSecondIdentifier] = useState("");
+
+  const [currencyCode, setCurrencyCode] = useState([{ symbol: "$" }]);
+  const [secondCurrencyCode, setSecondCurrencyCode] = useState([
+    { symbol: "$" },
+  ]);
+
+  const [imageAvailable, setImageAvailable] = useState(true);
+  const [secondImageAvailable, setsecondImageAvailable] = useState(true);
+
+  // Function for first component
+  function setCountry(e) {
+    const country = e.target.value;
+    const url = "https://countryflagsapi.com/svg/";
+    const filterCountryCode = countries.filter(
+      (cout) => cout.code === country.slice(0, 3)
+    );
+    setCurrencyCode(filterCountryCode);
+
+    setIdentifier(country.slice(0, 2));
+    setFlagURL(url + identifier);
+    setInputValue(country);
+    // check if flag image exists
+
+    const img = new Image();
+    img.src = url + identifier;
+
+    if (img.complete) {
+      setImageAvailable(true);
+    } else {
+      img.onload = () => {
+        setImageAvailable(true);
+      };
+
+      img.onerror = () => {
+        setImageAvailable(false);
+      };
+    }
+  }
+
+  // Function for second component
+  function setSecondCountry(e) {
+    const country = e.target.value;
+    const url = "https://countryflagsapi.com/svg/";
+    const filterCountryCode = countries.filter(
+      (cout) => cout.code === country.slice(0, 3)
+    );
+    setSecondCurrencyCode(filterCountryCode);
+
+    setSecondIdentifier(country.slice(0, 2));
+    setSecondFlagURL(url + secondIdentifier);
+    setSecondInputValue(country);
+    // check if flag image exists
+
+    const img = new Image();
+    img.src = url + secondIdentifier;
+
+    if (img.complete) {
+      setsecondImageAvailable(true);
+    } else {
+      img.onload = () => {
+        setsecondImageAvailable(true);
+      };
+
+      img.onerror = () => {
+        setsecondImageAvailable(false);
+      };
+    }
+  }
+
+  // Function to clear first input
+  function clearInput() {
+    setInputValue("");
+  }
+
+  // Function to clear second input
+  function secondClearInput() {
+    setSecondInputValue("");
+  }
+
+  function changeAmountInput(e) {
+    setAmountInput(e.target.value);
+  }
+  function changeAmountOutput(e) {
+    setAmountOutput(e.target.value);
+    setAmountInput();
+  }
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/Michaeloye/currencies-name-and-code/main/currency-list.json"
+      )
+      .then((res) => {
+        setCountries(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  useEffect(() => {
+    // axios
+    //   .get(
+    //     `${import.meta.env.VITE_APP_API_URL}want=${secondInputValue.slice(
+    //       0,
+    //       3
+    //     )}&have=${inputValue.slice(0, 3)}&amount=${amountInput}`,
+    //     headers
+    //   )
+    //   .then((res) => {
+    //     setAmountOutput(res.data.new_amount);
+    //     console.log(res.data);
+    //   });
+    axios
+      .get(
+        `https://v6.exchangerate-api.com/v6/${
+          headers["headers"]["X-Api-Key"]
+        }/pair/${inputValue.slice(0, 3)}/${secondInputValue.slice(0, 3)}`
+      )
+      .then((res) => setAmountOutput(amountInput * res.data.conversion_rate))
+      .catch((error) => console.log(error));
+  }, [inputValue, secondInputValue, amountInput]);
   return (
     <div className="h-screen">
       <Header />
@@ -13,7 +150,17 @@ function App() {
           <p className="text-primary-gray font-medium mt-3 md:mt-auto ml-5 mb-1 ">
             From
           </p>
-          <CurrencyCard />
+          <CurrencyCard
+            countryFlag={flagURL}
+            data={countries}
+            currencyCode={currencyCode}
+            inputValue={inputValue}
+            imageAvailable={imageAvailable}
+            setCountry={setCountry}
+            clearInput={clearInput}
+            amount={amountInput}
+            setAmount={changeAmountInput}
+          />
         </div>
         <div
           className="bg-primary-red h-12 w-12 mt-5 md:mt-0 flex justify-center items-center rounded-full 
@@ -23,7 +170,17 @@ function App() {
         </div>
         <div>
           <p className="text-primary-gray font-medium ml-5 mb-1 ">To</p>
-          <CurrencyCard />
+          <CurrencyCard
+            countryFlag={secondFlagURL}
+            data={countries}
+            currencyCode={secondCurrencyCode}
+            inputValue={secondInputValue}
+            imageAvailable={secondImageAvailable}
+            setCountry={setSecondCountry}
+            clearInput={secondClearInput}
+            amount={amountOutput}
+            setAmount={changeAmountOutput}
+          />
         </div>
       </div>
 
