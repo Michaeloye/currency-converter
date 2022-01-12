32db8976d579bbd22568e2c9e2b66d0c0566e1a3
michaeloye <michaeloye23@gmail.com>
2022-01-12 19:13:28 +0100

    remove previous logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,6 @@ function Header() {
   return (
     <div className="bg-primary-red h-10 flex items-center">
       <div className="ml-5">
-        {/* <h1 className="text-white text-2xl font-bold">XC</h1> */}
         <SiExpertsexchange color="white" size="27" />
       </div>
     </div>
