da5287c094e2a46da989d3081d636727b4244747
michaeloye <michaeloye23@gmail.com>
2022-01-12 19:05:46 +0100

    remove 'App.css' from imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import Header from "./components/header";
 import CurrencyCard from "./components/currencyCard";
 import { CgArrowsExchange } from "react-icons/cg";
